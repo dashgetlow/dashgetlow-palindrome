915b5dd61cbcc18995e09f2700b958901ed95634
dashiel <dashieluk@gmail.com>
2022-02-26 15:47:23 +0000

    Updated palindrome npm module to assert that an empty string is not a palindrome

--- index.js
+++ index.js
@@ -25,7 +25,12 @@ function Phrase(content) {
 
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
-    return this.processedContent() === this.processedContent().reverse();
+    // return this.processedContent() === this.processedContent().reverse();
+    if (this.processedContent()) {
+      return this.processedContent() === this.processedContent().reverse();
+    } else {
+      return false;
+    }
   }
 }
 
